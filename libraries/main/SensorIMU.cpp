73569f74a605f15ce82fdcc6b1502a6873e6e55d
carodavis-alt <carodavis@g.hmc.edu>
2026-03-25 21:00:17 -0700

    Update SensorIMU.cpp
    Add Magnometer Variable

--- libraries/main/SensorIMU.cpp
+++ libraries/main/SensorIMU.cpp
@@ -79,6 +79,9 @@ String SensorIMU::printRollPitchHeading(void) {
   printString += " heading: ";
   printString += String(state.heading);
   printString += "[deg]";
+  printString += " MagX: ";
+  printString += String(state.mx);
+  printString += "[deg]";
   return printString; 
 }
 
